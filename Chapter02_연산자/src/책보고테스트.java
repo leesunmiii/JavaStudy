6942a31cb31efb18b9e1537041f43ad2e5630708
SIST <SIST@DESKTOP-QUJ9LK4>
2023-09-14 15:15:55 +0900

    d

---
+++ Chapter02_연산자/src/책보고테스트.java
@@ -0,0 +1,15 @@
+
+public class 자바데이터형변경 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int _int = (int) 100.4464315;
+		System.out.println("_int:" + _int);
+
+		byte _byte = (byte) 300;
+		System.out.println("_byte:" + _byte);
+
+		System.out.println(0b00101100);
+	}
+
+}
